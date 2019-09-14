4ce8d6005aa46a38e1169974781472ff39c64bd7
Maasa <konomaasa@gmail.com>
2019-09-14 00:12:58 -0700

    Done.

--- app.rb
+++ app.rb
@@ -24,4 +24,28 @@ class App < Sinatra::Base
     result
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
+    word1 + " " + word2 + " " + word3 + " " + word4 + " "  + word5 + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if params[:operation] == "add"
+      result = number1 + number2
+    elsif params[:operation] == "subtract"
+      result = number1 - number2
+    elsif params[:operation] == "multiply"
+      result = number1 * number2
+    else params[:operation] == "divide"
+      result = number1 / number2
+    end
+    "#{result}"
+  end
+
 end
