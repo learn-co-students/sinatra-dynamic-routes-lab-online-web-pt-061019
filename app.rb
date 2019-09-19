3209e7e51a5adf70f2014b4b741a24d85695ad69
Jack <jackrrye@gmail.com>
2019-09-18 23:37:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -13,13 +13,14 @@ class App < Sinatra::Base
 
   get "/say/:number/:phrase" do 
     @num = params[:number].to_i
-    @num.times do 
+    @phrase = @num.times.map do |x|
       params[:phrase]
     end 
-  end 
+  	@phrase.join(" ")
+  end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do 
-    params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5]
+    params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
   end 
 
   get "/:operation/:number1/:number2" do 
